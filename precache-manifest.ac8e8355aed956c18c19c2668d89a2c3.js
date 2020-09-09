6ed009e2ef16ae006bfa498194f9c990bc355c1e
hobbyluv <princewillchime43@gmail.com>
2020-09-09 23:30:30 +0100

    Updates

---
+++ precache-manifest.ac8e8355aed956c18c19c2668d89a2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c23b3e12e17786697e6fa8906d96e2",
+    "url": "/covid_19_records/index.html"
+  },
+  {
+    "revision": "ccc625c2533933cc92dc",
+    "url": "/covid_19_records/static/css/main.cf13b1c1.chunk.css"
+  },
+  {
+    "revision": "84d856ffa234cd11c68c",
+    "url": "/covid_19_records/static/js/2.23bf10f7.chunk.js"
+  },
+  {
+    "revision": "1c35057aed30085c10c811a6c8dd623b",
+    "url": "/covid_19_records/static/js/2.23bf10f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc625c2533933cc92dc",
+    "url": "/covid_19_records/static/js/main.7a66cd47.chunk.js"
+  },
+  {
+    "revision": "b1a800b1db2fecdd005f",
+    "url": "/covid_19_records/static/js/runtime-main.260a8f2d.js"
+  },
+  {
+    "revision": "5f3ac40fa29743152360d648cc43afcc",
+    "url": "/covid_19_records/static/media/covidimage3.5f3ac40f.jpeg"
+  }
+]);
